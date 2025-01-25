1138dd6e08884a8c25bc473769d8955cc2db85a2
danidevx <deanarespe@gmail.com>
2025-01-25 22:15:32 +0100

    arrglar el problema del css con todos los revert

--- portafolio/veterinaria-farmavet/js/productnovedades.js
+++ portafolio/veterinaria-farmavet/js/productnovedades.js
@@ -3,17 +3,18 @@ const productListNovedades = document.getElementById('product-listrenovedades');
 
 // Ejemplo de productos novedades (puedes usar los mismos o diferentes)
 const productsNovedades = [
-    { name: 'SONENIX 150®', imageSrc: 'img/product-novedades/sonenix.png' },
-    { name: 'ENOMETROL®', imageSrc: 'img/product-novedades/Enometrol.png' },
-    { name: 'AMOXIGEN', imageSrc: 'img/product-novedades/amoxigen.png' },
-    { name: 'OXITETRACICLINA AL 5%', imageSrc: 'img/product-novedades/OXITETRACICLINA.png' },
-    { name: 'FLORFENICOL L.A.', imageSrc: 'img/product-novedades/FLORFENICOL.png' },
-    { name: 'SULFACET', imageSrc: 'img/product-novedades/SULFACET.png' }
-]
-;
+    { name: 'SONENIX 150®', imageSrc: 'img/product-novedades/sonenix.png', descripProdu: 'Para Bovinos, Porcinos, Caprinos y Ovinos en el tratamiento de mastitis infecciosa y pododermatitis en ganado bovino, tratamiento en infecciones respiratorias, urogenitales, gastrointestinales, de piel y tejidos blandos. Vía intramuscular profunda.' },
+    { name: 'ENOMETROL®', imageSrc: 'img/product-novedades/Enometrol.png', descripProdu: 'Para el tratamiento de infecciones respiratorias, genito-urinarias provocadas por bacterias gramnegativas y grampositivas. Infecciones de la cavidad oral y afecciones cutáneas. Vía oral. 0,2 ml por cada kg de peso vivo.' },
+    { name: 'AMOXIGEN', imageSrc: 'img/product-novedades/amoxigen.png', descripProdu: 'Bovinos: Infecciones causadas por bacterias sensibles a la combinación de Amoxicilina y Gentamicina, como las presentes en neumonías, enteritis bacteriana, mastitis, metritis y abscesos cutáneos.' },
+    { name: 'OXITETRACICLINA AL 5%', imageSrc: 'img/product-novedades/OXITETRACICLINA.png', descripProdu: 'Bovinos: para tratar Anaplasmosis, Lesiones Pódales, Leptospirosis, Metritis, sensibles a Oxitetraciclina. Porcinos: para tratar el Síndrome Mastitis Metritis Agalactia (MMA), Neumonía, Lesiones Pódales, sensibles a Oxitetraciclina. Ovinos: para tratar Pasteurelosis, Clamidiasis, Neumonía, Lesiones Pódales, sensibles a Oxitetraciclina. Aves: para tratar Coriza, Colera, Enfermedad Crónica Respiratoria (ECR), sensibles a Oxitetraciclina.' },
+    { name: 'FLORFENICOL L.A.', imageSrc: 'img/product-novedades/FLORFENICOL.png', descripProdu: 'Antibiótico de amplio espectro que actúa contra bacterias gram positivas y gram negativas del ganado bovino, ovino, caprino, porcino y aves, como: Pasteurellas, Haemophylus somnus, Klebsiella sp., Escherichia coli, Moraxella sp., Salmonella sp., Bacteroides sp., Fusobacterium sp., Corynebacterium pyogenes, Clostridium sp., Streptococcus, causantes de infecciones como neumonías, pododermatitis, metritis, enteritis y en general, procesos susceptibles a bacterias sensibles al Florfenicol.' },
+    { name: 'SULFACET', imageSrc: 'img/product-novedades/SULFACET.png', descripProdu: 'Sirve para combatir infecciones generales, agudas o crónicas, causadas por bacterias gram negativas o positivas, sensibles a la formulación en bovinos, equinos, ovinos, caprinos y porcinos. Esta indicado en Neumonías, Bronquitis Infecciosas, Difterias de los terneros, Enteritis Bacilar, Septicemia Hemorrágica, Metritis Séptica y Retención Placentaria; Mastitis Aguda, Poliartritis, Colibacilosis, Flemones y Abscesos, en infecciones secundarias de la Diarrea Blanca del ternero y como auxiliar en el tratamiento de la “Chinela» y en cualquier otra infección donde este indicado el tratamiento sulfamídico. ' }
+];
+
 
 // Agregar cada producto al DOM
 productsNovedades.forEach(product => {
-    const productElement = createProduct(product.name, product.imageSrc);
+    const productElement = createProduct(product.name, product.imageSrc, product.descripProdu);
     productListNovedades.appendChild(productElement);
 });
+
--- portafolio/veterinaria-farmavet/js/productrecomendamos.js
+++ portafolio/veterinaria-farmavet/js/productrecomendamos.js
@@ -3,16 +3,18 @@ const productListRecomendamos = document.getElementById('product-listrecomendamo
 
 // Ejemplo de productos recomendados
 const productsRecomendados = [
-    { name: 'ANTRIFORMINT FM ®', imageSrc: 'img/product/reme1.png' },
-    { name: 'ANTRIBAM 4G ®', imageSrc: 'img/product/atriban.png' },
-    { name: 'ASCEND', imageSrc: 'img/product/ascend.png' },
-    { name: 'ENROFLOXACINA 10% ®', imageSrc: 'img/product/enrofloxasina.png' },
-    { name: 'FLUXIPEN ®', imageSrc: 'img/product/fluxipen.png' },
-    { name: 'RESANIQ ®', imageSrc: 'img/product/RESANIQ.png' }
+    { name: 'ANTRIFORMINT FM ®', imageSrc: 'img/product/ANTRIFORMINT.png', descripProdu: 'Para el tratamiento de anaplasmosis y babesia en bovinos, búfalos, equinos, ovinos y caprinos. Antibiótico de amplio espectro. Polvo para reconstituir.' },
+    { name: 'ANTRIBAM 4G ®', imageSrc: 'img/product/atriban.png', descripProdu: 'Indicaciones: En bovinos, búfalos, ovinos y caprinos para la prevención, tratamiento y control de la tripanosomiasis. Dosis y Administración: Vía intramuscular profunda. Polvo para reconstituir. Agítese antes de usar.' },
+    { name: 'ASCEND', imageSrc: 'img/product/ascend.png', descripProdu: 'Indicaciones: Está indicado para la prevención y tratamiento de la piroplasmosis, anaplasmosis, ehrlichiosis, hepatozoonosis y citauxzoonosis. Dosis y Administración: Vía subcutánea o intramuscular. Bovinos, Ovinos y Caprinos: 1 - 2,5 ml por cada 100 kg de peso vivo. Equinos: 2 ml por cada 100 kg de peso vivo. Caninos: 0,5 ml por cada 10 kg de peso vivo. DOSIS TERAPÉUTICA: 1-5 mg ' },
+    { name: 'ENROFLOXACINA 10% ®', imageSrc: 'img/product/enrofloxasina.png', descripProdu: 'Indicaciones: Para tratamiento de procesos infecciosos del aparato respiratorio, digestivo y genitourinario. Infecciones virales e infecciones de la piel causadas por gérmenes grampositivos, gramnegativos y micoplasma en Bovinos, Ovinos, Caprinos, Porcinos, Felinos y Caninos. Dosis y Administración: Vía subcutánea, intramuscular o intravenosa. (en el anca). Las ovejas y las cabras se inyectan en los músculos del muslo. Bovinos, Ovinos, Caprinos y Porcinos: 1 ml por cada 40 kg de peso vivo. En casos graves, aplicar 1ml por cada 20 kg de peso vivo. DOSIS TERAPÉUTICA: 8-10 mg' },
+    { name: 'FLUXIPEN ®', imageSrc: 'img/product/fluxipen.png', descripProdu: 'Indicaciones: Antibiótico de rápido efecto y prolongada acción en infecciones causadas por gérmenes sensibles a la penicilina. Suspensión inyectable estéril. Dosis y Administración: Vía intramuscular profunda. Dosis General: 1 ml por 20 kg de peso vivo. Aplicar 3 ml por cada 50 - 100 de peso vivo. Tratamiento: de 1 a 3 días consecutivos. Después de reconstruir es estable por 30 días en refrigeración. DOSIS TERAPEUTICA: 11.000 - 22.000 UI' },
+    { name: 'RESANIQ ®', imageSrc: 'img/product/RESANIQ.png', descripProdu: '<h1>Indicaciones:</h1> <p>En bovinos, ovinos, caprinos y porcinos en el tratamiento preventivo y curativo de infecciones respiratorias (pasteurelosis, neumonía, bronconeumonía, pleuroneumonía), infecciones urogenitales (cistitis, nefritis, metritis, síndrome MMA de las cerdas), infecciones cutáneas y tejidos blandos (pododermatitis abscesos) e infecciones postparto</p> <br>. <h2>Dosis y Administración:</h2> <br> Vía intramuscular o subcutánea. Porcinos, Ovinos, Bovinos y Caprinos: 1 ml por cada 10 kg de peso vivo. Repetir la dosis a las 48 horas. DOSIS TERAPÉUTICA: 10 mg' },
+    { name: 'RESANIQ ®', imageSrc: 'img/product/RESANIQ.png', descripProdu: '<h1>hola</h1> <br> <h1>hola</h1>' }
+
 ];
 
 // Agregar cada producto al DOM
 productsRecomendados.forEach(product => {
-    const productElement = createProduct(product.name, product.imageSrc);
+    const productElement = createProduct(product.name, product.imageSrc, product.descripProdu);
     productListRecomendamos.appendChild(productElement);
 });
