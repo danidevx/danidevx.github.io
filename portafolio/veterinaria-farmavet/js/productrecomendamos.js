8e832a583d302d6c29b6806e5d4c54b9c1544686
danidevx <deanarespe@gmail.com>
2025-01-30 23:20:27 +0100

    arreglado de tarjeta de productos en todas las categorias

--- portafolio/veterinaria-farmavet/js/productrecomendamos.js
+++ portafolio/veterinaria-farmavet/js/productrecomendamos.js
@@ -3,14 +3,33 @@ const productListRecomendamos = document.getElementById('product-listrecomendamo
 
 // Ejemplo de productos recomendados
 const productsRecomendados = [
-    { name: 'ANTRIFORMINT FM ®', imageSrc: 'img/product/ANTRIFORMINT.png', descripProdu: '<h3>Indicaciones:</h3><br>Para el tratamiento de anaplasmosis y babesia en bovinos, búfalos, equinos, ovinos y caprinos. Antibiótico de amplio espectro. Polvo para reconstituir.<h3>Dosis y Administración:</h3><br>Vía intramuscular profunda. Bovinos, Búfalos, Ovinos y Caprinos: 1,5 ml por 20 kg de peso vivo. Equinos: dividir dosis y aplicar con intervalos de 2 horas para evitar síntomas de cólico.<h3>DOSIS TERAPÉUTICA:</h3><br>8-10 mg<h3>Composición y especies:</h3><br>Cada 100 cm³ contienen: Oxitetraciclina HCL 200 mg Antipirina 150 mg Diminazeno 70 mg Excipientes c.s.p. 100 ml' },
-    { name: 'ANTRIBAM 4G ®', imageSrc: 'img/product/atriban.png', descripProdu: '<h3>Indicaciones:</h3><br>En bovinos, búfalos, ovinos y caprinos para la prevención, tratamiento y control de la tripanosomiasis.<h3>Dosis y Administración:</h3><br>Vía intramuscular profunda. Polvo para reconstituir. Agítese antes de usar. <h4>Dosis preventiva:</h4> 1 ml por cada 80 kg de peso vivo. <h4>Dosis curativa:</h4> En zonas de baja incidencia 1 ml por cada 40 kg de peso vivo. En zonas de mediana o alta incidencia 2 ml por cada 40 kg de peso vivo.<h3>DOSIS TERAPÉUTICA:</h3><br>0,5 mg<h3>Composición y especies:</h3><br>Cada 100 cm³ contienen: Cloruro de isometamidium 4 g Diluyente estéril 200 ml' },
-    { name: 'ASCEND ®', imageSrc: 'img/product/ascend.png', descripProdu: '<h3>Indicaciones:</h3><br>Está indicado para la prevención y tratamiento de la piroplasmosis, anaplasmosis, ehrlichiosis, hepatozoonosis y citauxzoonosis.<h3>Dosis y Administración:</h3><br>Vía subcutánea o intramuscular. Bovinos, Ovinos y Caprinos: 1 - 2,5 ml por cada 100 kg de peso vivo. Equinos: 2 ml por cada 100 kg de peso vivo. Caninos: 0,5 ml por cada 10 kg de peso vivo.<h3>DOSIS TERAPÉUTICA:</h3><br>1-5 mg<h3>Composición y especies:</h3><br>Cada 100 cm³ contienen: Imidocarb Dipropionato 12 g Excipientes c.s.p. 100 ml' },
-    { name: 'ENROFLOXACINA 10% ®', imageSrc: 'img/product/enrofloxasina.png', descripProdu: '<h3>Indicaciones:</h3><br>Para tratamiento de procesos infecciosos del aparato respiratorio, digestivo y genitourinario. Infecciones virales e infecciones de la piel causadas por gérmenes grampositivos, gramnegativos y micoplasma en Bovinos, Ovinos, Caprinos, Porcinos, Felinos y Caninos.<h3>Dosis y Administración:</h3><br>Vía subcutánea, intramuscular o intravenosa. (en el anca). Las ovejas y las cabras se inyectan en los músculos del muslo. <h4>Bovinos, Ovinos, Caprinos y Porcinos: </h4> 1 ml por cada 40 kg de peso vivo. En casos graves, aplicar 1ml por cada 20 kg de peso vivo.<h3>DOSIS TERAPÉUTICA:</h3><br>8-10 mg<h3>Composición y especies:</h3><br>Cada 1 cm³ contienen: Enrofloxacina 100 mg Excipientes c.s.p. 1 ml' },
-    { name: 'FLUXIPEN ®', imageSrc: 'img/product/fluxipen.png', descripProdu: '<h3>Indicaciones:</h3><br>Antibiótico de rápido efecto y prolongada acción en infecciones causadas por gérmenes sensibles a la penicilina. Suspensión inyectable estéril.<h3>Dosis y Administración:</h3><br>Vía intramuscular profunda. Dosis General: 1 ml por 20 kg de peso vivo. Aplicar 3 ml por cada 50 - 100 de peso vivo. Tratamiento: de 1 a 3 días consecutivos. Después de reconstruir es estable por 30 días en refrigeración.<h3>DOSIS TERAPÉUTICA:</h3><br>11.000 - 22.000 UI<h3>Composición y especies:</h3><br>Cada 100 cm³ contienen: Oxitetraciclina HCL 200 mg Antipirina 150 mg Diminazeno 70 mg Excipientes c.s.p. 100 ml' },
-    { name: 'RESANIQ ®', imageSrc: 'img/product/RESANIQ.png', descripProdu: '<h3>Indicaciones:</h3><br>En bovinos, ovinos, caprinos y porcinos en el tratamiento preventivo y curativo de infecciones respiratorias (pasteurelosis, neumonía, bronconeumonía, pleuroneumonía), infecciones urogenitales (cistitis, nefritis, metritis, síndrome MMA de las cerdas), infecciones cutáneas y tejidos blandos (pododermatitis abscesos) e infecciones postparto.<h3>Dosis y Administración:</h3><br>Vía intramuscular o subcutánea. Porcinos, Ovinos, Bovinos y Caprinos: 1 ml por cada 10 kg de peso vivo. Repetir la dosis a las 48 horas.<h3>DOSIS TERAPÉUTICA:</h3><br>10 mg<h3>Composición y especies:</h3><br>Cada 100 cm³ contienen: Amoxicilina 15 g Excipientes C.S.P. 100 ml' }
+    {
+        name: 'GUSANACUR ®',
+        imageSrc: 'categorias/img/otro/gusanacur.png',
+        descripProdu: '<h3>Indicaciones:</h3><br>En Bovinos, Porcinos, Ovinos, Caprinos, Equinos, Caninos y Búfalos. Para el control y tratamiento de Miasis en heridas producidas por cortes, cirugías, descornes, castraciones, marcajes, desinfección de ombligo en recién nacidos. Como coadyuvante en la cicatrización de heridas.<h3>Dosis y Administración:</h3><br>Sólo uso tópico. Dirija la válvula hacia el sitio afectado a unos 30 cm de distancia y presione rociando el producto hasta cubrir totalmente el área afectada.<h3>Composición y especies:</h3> Cada 100 cm³ contienen: Clorofilos 5 g Aceite de pino 10 g Aceite de citronela 3 g Violeta de genciana 4 g Excipientes c.s.p. 100 ml Gas Propelente'
+    },
+    {
+        name: 'AMIGAR ®',
+        imageSrc: 'categorias/img/otro/AMIGAR.png',
+        descripProdu: '<h3>Indicaciones:</h3><br>Amigar es un baño Garrapaticida, componente de baja toxicidad para mamiferos. Producto tóxico para peces.<h3>Dosis y Administración:</h3><br>Únicamente para uso externo. Diluir 25 ml del producto en 20 litros de agua, mojar bien el animal y dejar que seque. Repetir a los 15 - 30 días contra garrapatas y contra la sarna una vez por semana hasta su curación. Preparar la solución inmediatamente antes de usarla y mantener el envase bien cerrado en un lugar fresco.<h3>Composición y especies:</h3> Cada 100 cm³ contienen: Amitraz 12.5 g Excipientes c.s.p. 100 ml'
+    },
+    {
+        name: 'CIPERMEDI ®',
+        imageSrc: 'categorias/img/otro/CIPERMEDI.png',
+        descripProdu: '<h3>Indicaciones:</h3><br>En el combate de los ectoparásitos que atacan a los bovinos e instalaciones avícolas. Tórsalo, o larvas de Dermafobia hominis. Moscas de los géneros Stomoxys calcitrans, Musca domestica, Haemofobia irritans - adultos; Dermafobia hominis - adultos y larva. Piojos de los géneros Linognathus vituli, Haematopinos eurysternus, Bovícola bovis, Solenopotes capillatus - adultos.<h3>Dosis y Administración:</h3><br>Únicamente para uso externo. Bovinos: 25 ml por cada 20 litros de agua. Diluir en la concentración indicada, agitar hasta la completa homogenización del baño. Bañar toda la superficie del animal, especialmente las regiones más infectadas, como papada, entrepiernas, orejas, etc. Usar aproximadamente 5 litros de baño por bovino adulto. Instalaciones avícolas: se utiliza un promedio de 1000 litros de la solución de Cipermedi para la fumigación de una instalación avícola de 1.200 m2. Recomienda un periodo de 48 horas para la recuperación del galpón por las aves.<h3>Composición y especies:</h3> Cada 100 cm³ contienen: Cipermetrina 15 g Cipertrifos 25 g Citronela 1 g Excipientes CSP 100 ml'
+    },
+    {
+        name: 'TINTURA DE YODO',
+        imageSrc: 'categorias/img/otro/TINTURA.png',
+        descripProdu: '<h3>Indicaciones:</h3><br>Germicida, Fungicida para desinfección de piel y curación de lesiones superficiales (heridas) en bovinos, equinos, porcinos, felinos, caninos, ovinos y caprinos.<h3>Dosis y Administración:</h3><br>Solución antiséptica de uso tópica. Aplicar de una a dos gotas sobre la lesión.<h3>Composición y especies:</h3><br>Cada 100 ml. contiene: Yodo Resublimado 2,50 g Yoduro de Potasio 1,66 g Excipientes c.s.p. 100 ml Bovinos, Equinos, Porcinos, Felinos, Caninos, Ovinos Y Caprinos.'
+    },
+    {
+        name: 'GEL MATAGUSANOS EQ 335',
+        imageSrc: 'categorias/img/otro/GEL.png',
+        descripProdu: '<h3>Indicaciones:</h3><br>Está indicado para ayudar a prevenir las infecciones en las heridas de los animales, y para el tratamiento local de infestaciones por larvas de gusano de monte. Tiene acción cicatrizante, repelente, insecticida y bactericida.<h3>Dosis y Administración:</h3><br>Tópica, aplicar mediante hisopo o brocha.<h3>Composición y especies:</h3><br>Ingredientes activos: 0.0 diethyl 0-3.5.6 trichol-2 pyridyl phosphorothioate a 96.5% 5% Aceite de pino 35% Ingredientes inertes 60% Bovino, equinos, caprinos, ovinos, porcinos, y caninos.'
+    },
     
-
+    
 ];
 
 // Agregar cada producto al DOM
@@ -18,4 +37,6 @@ productsRecomendados.forEach(product => {
     const productElement = createProduct(product.name, product.imageSrc, product.descripProdu);
     productListRecomendamos.appendChild(productElement);
 });
+
+
 
